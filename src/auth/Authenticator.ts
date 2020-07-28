cd7615cdba9b1d79a4c017d9e1b05da5e163e43c
Garrett Smith <garrett.smit7@gmail.com>
2020-07-28 17:18:34 -0400

    Add more explanation of why this is necessary

--- src/auth/Authenticator.ts
+++ src/auth/Authenticator.ts
@@ -1,5 +1,9 @@
 /**
- * An object that can authenticate a user and persist/maintain a user's authentication.
+ * An object that can authenticate a user and persist/maintain a user's authentication. Note that
+ * this is important because if we use the Authenticator type, we can make a MockAuthenticator or
+ * any other type of Authenticator that implements this interface. Then we can pass in any of those
+ * implementing classes as our authenticators used in a variety of different settings, like the
+ * signup and login forms.
  */
 export interface Authenticator {
   login: (password: string, email: string, onError: VoidFunction, onSuccess: VoidFunction) => void
