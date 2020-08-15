63d7841b68655baff0c47af0905950488779fc87
Garrett Smith <garrett.smit7@gmail.com>
2020-08-15 12:28:46 -0400

    abstract out map component

---
+++ src/components/MapboxMap.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
+
+/**
+ * A component that sets up the MapboxMap and provides a callback to act on the map during the
+ * initial rendering of the map. Map renders to 100% width and height of parent container.
+ */
+const MapboxMap = ({handleInitialRender}: {
+  handleInitialRender: (map: mapboxgl.Map) => void
+}) => {
+  const [map, setMap] = useState<mapboxgl.Map | null>(null);
+  const mapContainer = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (!process.env.MAPBOX_ACCESS_TOKEN) {
+      throw Error("A MapBox access token wasn't found in the environment variables.");
+    } else {
+      mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
+    }
+
+    const initializeMap = ({setMap, mapContainer}: {
+      setMap: React.Dispatch<React.SetStateAction<mapboxgl.Map | null>>,
+      mapContainer: React.MutableRefObject<HTMLElement | null>
+    }) => {
+      const map = new mapboxgl.Map({
+        // container expects HTMLElement | string. mapContainer.current should have the div element
+        // from below where it's assigned in the div. Therefore, it should never default to empty
+        // quotes.
+        container: mapContainer.current || "", 
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [0, 0],
+        zoom: 5
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
+
+      // Passed in through props
+      handleInitialRender(map);
+    }
+
+    if (!map) initializeMap({ setMap, mapContainer });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+      <div 
+        ref={el => {mapContainer.current = el}}
+        style={{
+          width: "100%",
+          height: "100%",
+          overflow: "visible!",
+        }} 
+      />
+  );
+}
+
+export default MapboxMap;
--- src/components/document-creation/PlaceMarker.tsx
+++ src/components/document-creation/PlaceMarker.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import "mapbox-gl/dist/mapbox-gl.css";
 import { DocumentCreationInputProps } from './DocumentCreationInputProps';
 import tw from 'twin.macro';
+import MapboxMap from '../MapboxMap';
 
 /**
  * The form screen where a user can place a marker, selecting a location for a region
@@ -12,8 +13,6 @@ import tw from 'twin.macro';
  * this page have been completed.
  */
 const PlaceMarker = ({documentBuilder, done}: DocumentCreationInputProps) => {
-  const [map, setMap] = useState<mapboxgl.Map | null>(null);
-  const mapContainer = useRef<HTMLElement | null>(null);
   const [marker, setMarker] = useState<mapboxgl.Marker | null>(() => {
     if (documentBuilder.coords) {
       const lat = documentBuilder.coords.lat;
@@ -22,55 +21,30 @@ const PlaceMarker = ({documentBuilder, done}: DocumentCreationInputProps) => {
     } else {
       return null;
     }
-  })
+  });
 
-  useEffect(() => {
-    if (!process.env.MAPBOX_ACCESS_TOKEN) {
-      throw Error("A MapBox access token wasn't found in the environment variables.");
-    } else {
-      mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
-    }
-
-    const initializeMap = ({setMap, mapContainer}: {
-      setMap: React.Dispatch<React.SetStateAction<mapboxgl.Map | null>>,
-      mapContainer: React.MutableRefObject<HTMLElement | null>
-    }) => {
-      const map = new mapboxgl.Map({
-        // container expects HTMLElement | string. mapContainer.current should have the div element
-        // from below where it's assigned in the div. Therefore, it should never default to empty
-        // quotes.
-        container: mapContainer.current || "", 
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [0, 0],
-        zoom: 5
-      });
-
-      map.on("load", () => {
-        setMap(map);
-        map.resize();
-        if (marker) {
-          marker.addTo(map);
-          done(true);
-        }
-      });
-
-      map.on("click", (ev) => {
+  const onInitMapRender = (map: mapboxgl.Map): void => {
+    map.on("load", () => {
+      if (marker) {
+        marker.addTo(map);
         done(true);
-        // Using callback inside setMarker because otherwise, this closure is stale and marker will
-        // always have its initial value: undefined. Using a callback ensures a fresh value is used.
-        setMarker(marker => {
-          if (marker) marker.remove();
-          documentBuilder.coords = {lng: ev.lngLat.lng, lat: ev.lngLat.lat};
+      }
+    });
 
-          return new mapboxgl.Marker()
-            .setLngLat([ev.lngLat.lng, ev.lngLat.lat])
-            .addTo(map)
-        })
-      });
-    }
+    map.on("click", (ev) => {
+      done(true);
+      // Using callback inside setMarker because otherwise, this closure is stale and marker will
+      // always have its initial value: undefined. Using a callback ensures a fresh value is used.
+      setMarker(marker => {
+        if (marker) marker.remove();
+        documentBuilder.coords = {lng: ev.lngLat.lng, lat: ev.lngLat.lat};
 
-    if (!map) initializeMap({ setMap, mapContainer });
-  }, [map, marker, documentBuilder, done]);
+        return new mapboxgl.Marker()
+          .setLngLat([ev.lngLat.lng, ev.lngLat.lat])
+          .addTo(map)
+      })
+    });
+  }
 
   return (
     <div style={tw`flex flex-col items-center`}>
@@ -78,12 +52,7 @@ const PlaceMarker = ({documentBuilder, done}: DocumentCreationInputProps) => {
         <h1>Choose a location for your article</h1>
         <h2>Click anywhere on the map to place a marker. Click again to re-place your marker.</h2>
       </div>
-      <div 
-        ref={el => {mapContainer.current = el}}
-        style={{
-          width: "80vw",
-          height: "80vh",
-        }}></div>
+      <MapboxMap handleInitialRender={onInitMapRender} />
     </div>
   );
 }
