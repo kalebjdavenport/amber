563657227ec3da72a94b84ea4e6c9409c3c77672
Garrett Smith <garrett.smit7@gmail.com>
2020-08-16 13:43:47 -0400

    Remove unused components

--- src/components/stories/NewHistory.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NewHistory } from '../NewHistory';
-
-export default {
-  component: NewHistory,
-  title: 'NewHistory',
-}
-
-export const Default = () => <NewHistory />
